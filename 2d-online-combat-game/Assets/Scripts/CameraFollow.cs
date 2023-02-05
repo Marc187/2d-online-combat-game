3198fbfba4e303ef0718bd6eb29e9c156d94ac21
tonybbb2 <89655535+tonybbb2@users.noreply.github.com>
2023-02-05 15:10:54 -0500

    camera follow script

--- 2d-online-combat-game/Assets/Scripts/CameraFollow.cs
+++ 2d-online-combat-game/Assets/Scripts/CameraFollow.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public float FollowSpeed = 2f;
+    public float yOffSet = 1f;
     public Transform target;
 
-
-    // Update is called once per frame
-    void LateUpdate()
+    private void Start()
     {
-        transform.position = target.transform.position;
+        target = GameObject.FindGameObjectWithTag("PrototypeHeroNew(Clone)").transform;
     }
+
+    private void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffSet, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+
+
+
 }
+
+
+
