71df61c7d6b6f881723d23a1680c8798296cd97a
Marc Charlebois <CharleboisMarc@hotmail.com>
2023-02-09 21:04:02 -0500

    Fix Timer Starts 1 second off

--- 2d-online-combat-game/Assets/Scripts/Timer.cs
+++ 2d-online-combat-game/Assets/Scripts/Timer.cs
@@ -34,7 +34,7 @@ public class Timer : MonoBehaviour
     }
     void DisplayTime(float timeToDisplay)
     {
-        timeToDisplay += 1;
+        //timeToDisplay += 1;
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
         timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
